774d7bcac8c40db92fc242385d42396327f38982
unknown <jaiswalneha2711@gmail.com>
2022-02-26 15:42:32 +0530

    Add NewPaletteForm and Link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Palette from './Palette';
 import seedColor from './seedColor';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 import { Route, Switch } from 'react-router-dom';
 import { generatePalette } from './ColorHelper';
 
@@ -17,17 +18,21 @@ class App extends Component{
   render(){
     return(
          <Switch>
-              <Route exact path="/" render ={(routeProps) => <PaletteList paletteList ={seedColor}  {...routeProps} /> } />
+              
+              <Route exact path = "/palette/new"  render={() => <NewPaletteForm /> }/>              
 
-
-              <Route exact path="/palette/:id" render ={(routeProps) => 
-                  <Palette palette = {generatePalette(this.findPalette(routeProps.match.params.id) )}
-               />} 
-
-               />
               <Route exact path="/palette/:paletteId/:colorId" 
                 render ={(routeProps) => <SingleColorPalette colorId = {routeProps.match.params.colorId} palette = {generatePalette(this.findPalette(routeProps.match.params.paletteId) )}
                />}  />
+
+              <Route exact path="/" render ={(routeProps) => <PaletteList paletteList ={seedColor}  {...routeProps} /> } />
+
+              <Route exact path="/palette/:id" render ={(routeProps) => 
+                  <Palette palette = {generatePalette(this.findPalette(routeProps.match.params.id) )}
+               />}/>
+
+                
+             
            </Switch>
          
       //     // <div>
