c664182051feeccda975bc898c3ff48c94620d16
unknown <jaiswalneha2711@gmail.com>
2022-03-05 17:08:36 +0530

    Saved palette to local storage

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import { generatePalette } from './ColorHelper';
 class App extends Component{
   constructor(props){
     super(props);
-    this.state = {palettes : seedColor}
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = {palettes : savedPalettes || seedColor};
     this.savePalette =this.savePalette.bind(this);
     this.findPalette =this.findPalette.bind(this);
 
@@ -23,7 +24,15 @@ class App extends Component{
     });
   }
   savePalette(newPalette){
-    this.setState({palettes: [...this.state.palettes, newPalette]})
+    this.setState({palettes: [...this.state.palettes, newPalette]},
+      this.syncLocalStorage
+       );
+  }
+  syncLocalStorage(){
+      window.localStorage.setItem(
+        "palettes",
+        JSON.stringify(this.state.palettes)
+      );
   }
   render(){
     return(
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -7,7 +7,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
 import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 import Button from '@material-ui/core/Button';
 import PaletteMetaForm from "./PaletteMetaForm";
@@ -39,7 +38,7 @@ class PaletteFormNav extends Component{
       }
     render(){
         const {classes, open, palettes, handleSubmit} = this.props;
-        const {newPaletteName} = this.state;
+        // const {newPaletteName} = this.state;
         return(
             <div className={classes.root}>
                 <CssBaseline />
@@ -57,10 +56,9 @@ class PaletteFormNav extends Component{
                         onClick={this.props.handleDrawerOpen}
                         edge="start"
                         className={clsx(classes.menuButton,{
-                        [ classes.hide]: open,
+                        [classes.hide]: open,
                          })}
                     >
-                        {/* <MenuIcon /> */}
                         <AddToPhotosIcon/>
                     </IconButton>
                     <Typography variant="h6" noWrap>
