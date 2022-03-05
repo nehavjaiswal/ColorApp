2b614712c28c963d4afe16b977a57b994ac90f81
unknown <jaiswalneha2711@gmail.com>
2022-03-05 23:15:05 +0530

    Add reponsive helper and styles colorBox

--- src/Styles/ColorBoxStyle.js
+++ src/Styles/ColorBoxStyle.js
@@ -1,5 +1,5 @@
 import chroma from "chroma-js";
-
+import sizes from "./Sizes";
 export default {
     colorBox : {
         height : props => (props.showingFullPalette ? "25%" : "50%"),
@@ -12,7 +12,20 @@ export default {
         "&:hover button":{
             opacity: "1",
             transition: "0.5s"
-        }
+        },
+        [sizes.down("lg")] : {
+            width: "25%",
+            height : props => (props.showingFullPalette ? "20%" : "10%"),
+        },
+        [sizes.down("md")] : {
+            width: "50%",
+            height : props => (props.showingFullPalette ? "10%" : "50%"),
+            },
+        [sizes.down("xs")] : {
+            width: "100%",
+            height : props => (props.showingFullPalette ? "5%" : "10%"),
+        },
+       
     },
     copyText :{
         color : props => chroma(props.background).luminance() >= 0.7 ? "black" : "white"
---
+++ src/Styles/Sizes.js
@@ -0,0 +1,31 @@
+
+// // Small devices (landscape phones, 576px and up)
+// @media (min-width: 576px) { ... }
+
+// // Medium devices (tablets, 768px and up)
+// @media (min-width: 768px) { ... }
+
+// // Large devices (desktops, 992px and up)
+// @media (min-width: 992px) { ... }
+
+// // X-Large devices (large desktops, 1200px and up)
+// @media (min-width: 1200px) { ... }
+
+// // XX-Large devices (larger desktops, 1400px and up)
+// @media (min-width: 1400px) { ... }
+
+export default {
+    up(){
+
+    },
+    down(size){
+        const sizes = {
+            xs:"576px",
+            sm : "768px",
+            md : "992px",
+            lg : "1200px",
+            xlg : "1400px"
+        }
+        return `@media (max-width: ${sizes[size]})`
+    }
+}
